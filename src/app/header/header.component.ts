c2b870c9aac9f2d948588de8129850e2eac9d347
umeshror <sarukumesh@gmail.com>
2019-12-22 10:52:02 +0530

    nav navigation added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core'
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() navSelectedEmitter = new EventEmitter<string>();
 
+  navSelected(selected: string) {
+    this.navSelectedEmitter.emit(selected);
+  }
 }
