5627e93391b7ba0ff7a5a8d4e606ef886c577d85
umeshror <sarukumesh@gmail.com>
2019-12-22 22:44:31 +0530

    added ingredients in recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,27 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from "../shared/models/ingredient.model";
 
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
 
   private recipes = [new Recipe(
     'A Test Recipe',
-    'description',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s'
+    'Lorem Ipsum',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s',
+    [
+      new Ingredient('Bread', 12),
+      new Ingredient('Eggs', 6),
+    ]
   ),
     new Recipe(
       'Recipe 2',
-      'Recipe 2',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s'
+      'Lorem Ipsum',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s',
+      [
+        new Ingredient('Capsicum', 2),
+        new Ingredient('Tomato', 2),
+      ]
     )];
 
   getRecipes() {
