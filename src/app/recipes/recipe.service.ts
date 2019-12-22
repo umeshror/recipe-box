999ac964ab96302b2c86f3a608ec36b4935b62dd
umeshror <sarukumesh@gmail.com>
2019-12-22 18:22:52 +0530

    RecipeService added

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,19 @@
+import {Recipe} from "./recipe.model";
+
+export class RecipeService {
+  private recipes = [new Recipe(
+    'A Test Recipe',
+    'description',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s'
+  ),
+    new Recipe(
+      'Recipe 2',
+      'Recipe 2',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s'
+    )];
+
+    getRecipes(){
+      // return new array copy of this one
+      return this.recipes.slice();
+    }
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,18 +1,23 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {RecipeService} from "./recipe.service";
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService],
+
 })
 export class RecipesComponent implements OnInit {
+  recipes : Recipe[];
   recipeItemSelected: Recipe;
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   recipeSelected(recipe: Recipe) {
