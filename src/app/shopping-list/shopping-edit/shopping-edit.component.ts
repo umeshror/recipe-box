dfd4da9071152bde780bc6cf994885452bbfa934
umeshror <sarukumesh@gmail.com>
2019-12-22 13:06:49 +0530

    added onIngredientAdded flow

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/models/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInputField', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInputField', {static: true}) amountInputRef: ElementRef;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  addIngredient() {
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    const ingredient = new Ingredient(name, amount);
+    this.ingredientAdded.emit(ingredient);
   }
 
 }
