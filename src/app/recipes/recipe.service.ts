538006ed5e10334f93955f5c518020d70c419a0a
umeshror <sarukumesh@gmail.com>
2019-12-22 23:02:40 +0530

    added ingredients in recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Ingredient} from "../shared/models/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   public recipeSelected = new EventEmitter<Recipe>();
 
@@ -24,9 +26,16 @@ export class RecipeService {
       ]
     )];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     // return new array copy of this one
     return this.recipes.slice();
+  }
+
+  addIngreToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 
 }
