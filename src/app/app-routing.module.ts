6c64c98f5ce150a7f45c338ba473fd6e04f9d5b0
umeshror <sarukumesh@gmail.com>
2019-12-23 15:25:08 +0530

    added appRoutes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RecipeListComponent} from "./recipes/recipe-list/recipe-list.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipesComponent} from "./recipes/recipes.component";
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent},
+];
+
+
+@NgModule({
+  imports: [
+    // RouterModule.forRoot(appRoutes, {useHash: true})
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
@@ -12,6 +13,8 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from "./shared/directives/dropdown.directive";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
@@ -27,10 +30,12 @@ import {ShoppingListService} from "./shopping-list/shopping-list.service";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
