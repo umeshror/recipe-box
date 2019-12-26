132707e52473d3e4a49e9e3b63e0dcd097436f86
umeshror <sarukumesh@gmail.com>
2019-12-26 17:08:09 +0530

    header menu handled

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RecipeApiService} from "../recipes/recipe-api.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private recipeApiService: RecipeApiService) {
+export class HeaderComponent implements OnInit {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
+  constructor(private recipeApiService: RecipeApiService,
+              private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
   }
 
   saveData() {
