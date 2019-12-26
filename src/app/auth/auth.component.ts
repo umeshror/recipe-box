2240841894c6b8b1fe3496ee33a7eb013f34f55d
umeshror <sarukumesh@gmail.com>
2019-12-26 12:17:56 +0530

    loading-spinner added in auth form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {NgForm} from '@angular/forms';
-import {AuthService} from "./auth.service";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,9 +9,10 @@ import {AuthService} from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor(private authService: AuthService) {
-  }
+  constructor(private authService: AuthService) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -22,10 +24,24 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signup(email, password).subscribe(
-      (responseData) => {
-        console.log(responseData);
-      });
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage;
+          this.isLoading = false;
+        }
+      );
+    }
+
     form.reset();
   }
 }
