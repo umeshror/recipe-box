1c5d051bb840c927488d756fe9345f8c265ffda6
umeshror <sarukumesh@gmail.com>
2019-12-26 12:02:44 +0530

    AuthService added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  templateUrl: './auth.component.html'
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
+  isLoginMode = true;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService) {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.signup(email, password).subscribe(
+      (responseData) => {
+        console.log(responseData);
+      });
+    form.reset();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,24 +8,7 @@ import {Subject} from 'rxjs';
 export class RecipeService {
   recipesUpdated = new Subject<Recipe>();
 
-  private recipes = [new Recipe(
-    'A Test Recipe',
-    'Lorem Ipsum',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s',
-    [
-      new Ingredient('Bread', 12),
-      new Ingredient('Eggs', 6),
-    ]
-  ),
-    new Recipe(
-      'Recipe 2',
-      'Lorem Ipsum',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ApfyIa9745tasU5E7H_CCdN_1FQ57VNdLaNXDUzipqgjeowztw&s',
-      [
-        new Ingredient('Capsicum', 2),
-        new Ingredient('Tomato', 2),
-      ]
-    )];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
