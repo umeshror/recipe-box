743d0963b60f55963faf272f6920e32658625450
umeshror <sarukumesh@gmail.com>
2019-12-31 13:42:54 +0530

    webpack dependencies added

---
+++ webpack.config.common.js
@@ -0,0 +1,29 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ngw = require('@ngtools/webpack');
+
+module.exports = {
+    entry: './src/app/main.ts',
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loaders: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['raw-loader']
+            }
+        ],
+        exprContextCritical: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
+
+
